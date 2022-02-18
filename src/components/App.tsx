c1dfcd3280ddc58532e33889c7af2e577a413de1
KronosGR <geo.elgeo@gmail.com>
2022-02-18 06:43:34 +0100

    connected component to redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Todo } from '../actions';
+import { fetchTodos } from '../actions/index';
+import { StoreState } from '../reducers';
 
- export class App extends React.Component{
-   render(){
-     return <div>Hello</div>
-   }
- }
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+export class _App extends React.Component<AppProps> {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+const mapStatoToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStatoToProps, {fetchTodos})(_App)
