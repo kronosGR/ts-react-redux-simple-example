d6dd0534cf3def111f9cfe8d8e10caf53cac0918
KronosGR <geo.elgeo@gmail.com>
2022-02-18 09:18:10 +0100

    final

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Todo } from '../actions';
+import { deleteTodo, Todo } from '../actions';
 import { fetchTodos } from '../actions/index';
 import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
-export class _App extends React.Component<AppProps> {
+
+interface AppState {
+  fetching: boolean;
+}
+
+export class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { fetching: false };
+  }
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({ fetching: true });
+  };
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
   };
 
   renderList(): JSX.Element[] {
     return this.props.todos.map((todo: Todo) => {
-      return <div key={todo.id}>{todo.title}</div>;
+      return (
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+          {this.state.fetching ? ' LOADING' : null}
+          {todo.title}
+        </div>
+      );
     });
   }
 
@@ -29,8 +57,8 @@ export class _App extends React.Component<AppProps> {
   }
 }
 
-const mapStatoToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
 
-export const App = connect(mapStatoToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
